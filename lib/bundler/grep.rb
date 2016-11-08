3ea462354e99c9d902350f4a1c2e109698532506
takkanm <takkanm@gmail.com>
2016-11-08 21:28:20 +0900

    grep files if you like command

--- lib/bundler/grep.rb
+++ lib/bundler/grep.rb
@@ -3,8 +3,20 @@ require 'bundler'
 
 module Bundler
   module Grep
-     def self.start!(argv)
-       p Bundler.load.specs
-     end
+    class << self
+      def start!(argv)
+        Kernel.exec(*grep_command, argv[0], *Bundler.load.specs.map(&:full_gem_path))
+      end
+
+      private
+
+      def grep_command
+        if ENV['BUNDLER_GREP_CMD']
+          ENV['BUNDLER_GREP_CMD'].split(/ /).reject {|w| w == '' }
+        else
+          %w(grep -R)
+        end
+      end
+    end
   end
 end

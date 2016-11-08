8bea60658337f6948d35959194bb45729181552e
takkanm <takkanm@gmail.com>
2016-11-08 20:58:27 +0900

    init

---
+++ lib/bundler/grep.rb
@@ -0,0 +1,7 @@
+require "bundler/grep/version"
+
+module Bundler
+  module Grep
+    # Your code goes here...
+  end
+end
---
+++ lib/bundler/grep/version.rb
@@ -0,0 +1,5 @@
+module Bundler
+  module Grep
+    VERSION = "0.1.0"
+  end
+end

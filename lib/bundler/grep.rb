9c5e6c308c3663a718ea554bed0ea692ca48e017
takkanm <takkanm@gmail.com>
2016-11-09 08:50:59 +0900

    add --gems option

--- lib/bundler/grep.rb
+++ lib/bundler/grep.rb
@@ -10,20 +10,40 @@ module Bundler
 
     class Command
       def initialize(argv)
-        @argv = argv
+        parse_argument!(argv)
       end
 
       def start!
-        Kernel.exec(*grep_command, @argv[0], *Bundler.load.specs.map(&:full_gem_path))
+        Kernel.exec(*grep_command, @argv[0], *selected_gem_paths)
       end
 
       private
+
+      def parse_argument!(argv)
+        opt = OptionParser.new
+        opt.on('-g name1[,name2..]', '--gems name1[,name2..]', 'select target gems') do |gems|
+          @gems = gems.split(',')
+        end
+
+        opt.parse!(argv)
+        @argv = argv
+      end
 
       def grep_command
         if ENV['BUNDLER_GREP_CMD']
           ENV['BUNDLER_GREP_CMD'].split(/ /).reject {|w| w == '' }
         else
           %w(grep -R)
+        end
+      end
+
+      def selected_gem_paths
+        if @gems
+          Bundler.load.specs.find_all {|spec|
+            @gems.include?(spec.name)
+          }.map(&:full_gem_path)
+        else
+          Bundler.load.specs.map(&:full_gem_path)
         end
       end
     end
